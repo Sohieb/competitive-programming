1541b47d27e215b6d57ac9c487993fa45de64673
Sohieb <ibrahim.abdelrahman95@gmail.com>
2018-02-12 19:10:36 +0800

    warm up

---
+++ Codeforces/A879-Borya's Diagnosis.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+
+#define F        first
+#define S        second
+#define pnl      printf("\n")
+#define sz(x)    (int)x.size()
+#define sf(x)    scanf("%d",&x)
+#define pf(x)    printf("%d\n",x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+#define FOR(a,b) for(int i = a; i < b; ++i)
+
+const db eps = 1e-12;
+const db pi = acos(-1);
+const int inf = 0x3f3f3f3f;
+const ll INF = inf * 2LL * inf;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n, a, d, out;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+//freopen("out.txt", "w", stdout);
+#endif
+  sf(n);
+  sf(a), sf(d);
+  out = a;
+  for(int i = 1; i < n; ++i){
+    sf(a), sf(d);
+    while(a <= out)
+      a += d;
+    out = a;
+  }
+  pf(out);
+  return 0;
+}

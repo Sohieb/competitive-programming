24d85893bc90a7d5282bc3a4c1941a189e443d15
Sohieb <ibrahim.abdelrahman95@gmail.com>
2018-02-20 15:23:56 +0800

    warm up

---
+++ Codeforces/warm up/A755-PolandBall and Hypothesis.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+
+#define F        first
+#define S        second
+#define pnl      printf("\n")
+#define sz(x)    (int)x.size()
+#define sf(x)    scanf("%d",&x)
+#define pf(x)    printf("%d\n",x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+#define FOR(a,b) for(int i = a; i < b; ++i)
+
+const db eps = 1e-12;
+const db pi = acos(-1);
+const int inf = 0x3f3f3f3f;
+const ll INF = inf * 2LL * inf;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n;
+
+bool prime(int x){
+  if(x < 2)return 0;
+  for(int i = 2; i * i <= x; ++i)
+    if(x % i == 0)
+      return 0;
+  return 1;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+//freopen("out.txt", "w", stdout);
+#endif
+  sf(n);
+  for(int i = 1; i <= 1000; ++i){
+    if(prime(i * n + 1))
+      continue;
+    printf("%d\n", i);
+    return 0;
+  }
+  return 0;
+}

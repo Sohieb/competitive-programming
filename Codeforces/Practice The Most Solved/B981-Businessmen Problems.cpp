bd0f9ab6a7e74a4f6c121a0f914fc7e76dec5cb7
Sohieb <ibrahim.abdelrahman95@gmail.com>
2018-07-06 02:08:35 +0800

    easy greedy (dif 2)

---
+++ Codeforces/Practice The Most Solved/B981-Businessmen Problems.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-12;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n, a, x;
+map<int, int> mp;
+
+void read(int N){
+    for(int i = 0; i < N; ++i){
+        sf(a), sf(x);
+        mp[a] = max(mp[a], x);
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    sf(n);
+    read(n);
+    sf(n);
+    read(n);
+    ll ans = 0;
+    for(auto v : mp)
+        ans += v.S;
+    cout << ans << endl;
+    return 0;
+}

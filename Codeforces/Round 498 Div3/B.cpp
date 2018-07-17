31e2bbe02402eb6c2c28f6a7be952374f9442fb6
Sohieb <ibrahim.abdelrahman95@gmail.com>
2018-07-17 21:59:43 +0800

    just practice

---
+++ Codeforces/Round 498 Div3/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n, k;
+vector<pii> a;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    cin >> n >> k;
+    a.resize(n);
+    for(int i = 0; i < n; ++i){
+        scanf("%d", &a[i].F);
+        a[i].S = 1;
+    }
+    while(k++ < n){
+        int mn = 0;
+        for(int i = 0; i < sz(a); ++i)
+            if(a[i].F < a[mn].F) mn = i;
+        if(mn > 0) a[mn - 1].S += a[mn].S;
+        else a[mn + 1].S += a[mn].S;
+        a.erase(a.begin() + mn);
+    }
+    int res = 0;
+    for(int i = 0; i < sz(a); ++i)
+        res += a[i].F;
+    printf("%d\n", res);
+    for(int i = 0; i < sz(a); ++i)
+        printf("%d%c", a[i].S, " \n"[i == sz(a) - 1]);
+    return 0;
+}

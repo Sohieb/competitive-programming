f81b412f86d82338ead2b6ac9fc8c45577e01d5e
Sohieb <ibrahim.abdelrahman95@gmail.com>
2018-08-08 18:51:29 +0800

    just practice

---
+++ Codeforces/Round 496 Div3/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n, a;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    scanf("%d%d", &n, &a);
+    int lst = 1;
+    vector<int> ans;
+    for(int i = 1; i < n; ++i){
+        scanf("%d", &a);
+        if(a == 1)
+            ans.push_back(lst);
+        lst = a;
+    }
+    ans.push_back(lst);
+    printf("%d\n", sz(ans));
+    for(int i = 0; i < sz(ans); ++i)
+        printf("%d%c", ans[i], " \n"[i==sz(ans)-1]);
+    return 0;
+}

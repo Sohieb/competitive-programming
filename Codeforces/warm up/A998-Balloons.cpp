cdedeb7ac3089800811c6bb358f3779161ab51c5
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-08-25 23:26:56 +0800

    warm up

---
+++ Codeforces/warm up/A998-Balloons.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n;
+pii a[11];
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &a[i].F);
+        a[i].S = i;
+    }
+    if (n == 1 || (n == 2 && a[0].F == a[1].F))
+        return !printf("%d\n", -1);
+    sort(a, a + n);
+    printf("%d\n", n - 1);
+    for(int i = 1; i < n; ++i)
+        printf("%d%c", a[i].S + 1, " \n"[i==n-1]);
+    return 0;
+}

d24e393d6f6321bfa20a6b7f6598d08aecf04e1d
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-12-26 00:45:25 +0800

    warm up

---
+++ Kattis/Pot.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n;
+int p;
+
+int pwr(int base, int pw) {
+    int ans = 1;
+    while (pw--) ans *= base;
+    return ans;
+}
+
+int main() {
+    cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> p;
+        res += pwr(p / 10, p % 10);
+    }
+    cout << res << endl;
+    return 0;
+}

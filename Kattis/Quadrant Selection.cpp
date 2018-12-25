62d0ca4da1681fe0973fc1444c604ce350baa4a6
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-12-26 00:40:44 +0800

    warm up

---
+++ Kattis/Quadrant Selection.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int x, y;
+
+int main() {
+    cin >> x >> y;
+    if (x > 0 && y > 0) cout << "1\n";
+    if (x < 0 && y > 0) cout << "2\n";
+    if (x < 0 && y < 0) cout << "3\n";
+    if (x > 0 && y < 0) cout << "4\n";
+    return 0;
+}

414f9d06da0a8b388022ef46b5fe0bcdd729c39b
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2018-12-26 17:39:45 +0800

    diff 2 (1st submission)

---
+++ Solo Code Sprint #01/B946-Weird Subtraction Process.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+ll a, b;
+
+int main() {
+    cin >> a >> b;
+    while (a > 0 && b > 0) {
+        if (a >= 2 * b) {
+            a %= 2 * b;
+        } else if (b >= 2 * a) {
+            b %= 2 * a;
+        } else break;
+    }
+    cout << a << " " << b << endl;
+    return 0;
+}

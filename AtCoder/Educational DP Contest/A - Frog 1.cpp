019335f72632e49a55ef92c1a7ca31199e93bc91
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-01-07 03:14:50 +0800

    Practicing DP problems

---
+++ AtCoder/Educational DP Contest/A - Frog 1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int n, a[100100];
+int dp[100100];
+
+int solve(int i) {
+    if (i == n) return 0;
+    int &ret = dp[i];
+    if (~ret) return ret;
+    ret = solve(i + 1) + abs(a[i] - a[i + 1]);
+    if (i + 2 <= n) ret = min(ret, solve(i + 2) + abs(a[i] - a[i + 2]));
+    return ret;
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
+        scanf("%d", &a[i]);
+    memset(dp, -1, sizeof dp);
+    printf("%d\n", solve(1));
+    return 0;
+}

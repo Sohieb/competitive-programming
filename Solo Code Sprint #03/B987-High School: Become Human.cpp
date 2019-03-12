eaee97380b01c39c8b64774d95e96210562b3cbf
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-03-12 21:39:44 +0800

    diff 1 (solved 3rd submission due to precision error)

---
+++ Solo Code Sprint #03/B987-High School: Become Human.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-12;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+ll x, y;
+
+int main() {
+    cin >> x >> y;
+    long double X = y * log(x);
+    long double Y = x * log(y);
+    if (abs(X - Y) <= eps) {
+        printf("=\n");
+        return 0;
+    }
+    if (X > Y) printf(">\n");
+    else printf("<\n");
+    return 0;
+}

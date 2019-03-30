a374bfc14ad460d88f53a2b9fbb69ecff53de13a
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-03-30 20:18:25 +0800

    diff 3

---
+++ Kattis/Block Game.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+ll n, m;
+
+int solve(ll a, ll b) {
+    if (a < b) swap(a, b);
+    if (a % b == 0) return 1;
+    if (a / b > 1) return 1;
+    return !solve(a - b, b);
+}
+
+int main() {
+    cin >> n >> m;
+    cout << (solve(n, m) ? "win\n" : "lose\n");
+    return 0;
+}

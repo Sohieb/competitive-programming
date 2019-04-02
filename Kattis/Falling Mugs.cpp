52af759bff5d1de9c436ff5c354027e2b3e33345
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-04-02 18:15:10 +0800

    diff 3.3

---
+++ Kattis/Falling Mugs.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int d;
+
+int main() {
+    cin >> d;
+    for (int i = 0; i <= d; ++i) {
+    	ll n1 = i * 1LL * i;
+    	ll n2 = n1 - d;
+    	int sq = int(sqrt(n2));
+    	if (sq * 1LL * sq == n2 && i != sq) 
+    		return !printf("%d %d\n", sq, i);
+    }
+    printf("impossible\n");
+    return 0;
+}

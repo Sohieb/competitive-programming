0c8fc8233457bc129084cc271a81702700c0b23c
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-04-07 17:48:43 +0800

    diff 1.5

---
+++ Kattis/Reverse Rot.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.", str;
+int n;
+
+int main() {
+    while (scanf("%d", &n), n > 0) {
+    	cin >> str;
+    	reverse(all(str));
+    	for (int i = 0; i < sz(str); ++i) {
+    		str[i] = alph[(alph.find(str[i]) + n) % sz(alph)];
+    	}
+    	cout << str << endl;
+    }
+    return 0;
+}

27b469834acefd8dbc8b13303b7c5630a2deaebc
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-04-07 17:25:51 +0800

    diff 5

---
+++ Kattis/Beautiful Primes.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+typedef double db;
+typedef long long ll;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+#define F          first
+#define S          second
+#define pnl        printf("\n")
+#define sz(x)      (int)x.size()
+#define sf(x)      scanf("%d",&x)
+#define pf(x)      printf("%d\n",x)
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define rep(i, n)  for(int i = 0; i < n; ++i)
+
+const db eps = 1e-9;
+const db pi = acos(-1);
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1000 * 1000 * 1000 + 7;
+
+int T, n;
+bool isPrime[1000000];
+vector<int> primes;
+
+void sieve(int N) {
+	isPrime[0] = isPrime[1] = 1;
+	for (int i = 2; i < N; ++i) {
+		if (isPrime[i]) continue;
+		primes.push_back(i);
+		for (int j = i + i; j < N; j += i)
+			isPrime[j] = 1;
+	}
+}
+
+int main() {
+	sieve(1000000);
+    scanf("%d", &T);
+    while (T--) {
+    	scanf("%d", &n);
+    	vector<int> ans;
+    	double d = 0;
+    	for (int i = 1; i <= n; ++i) {
+    		int l = 0, r = sz(primes) - 1, mid;
+    		while (l < r) {
+    			mid = l + (r - l+ 1) / 2;
+    			if (d + log10(primes[mid]) < i) l = mid;
+    			else r = mid - 1;
+    		}
+    		ans.push_back(primes[l]);
+    		d += log10(primes[l]);
+    	}
+    	for (int i = 0; i < n; ++i)
+    		printf("%d%c", ans[i], " \n"[i == n - 1]);
+    }
+    return 0;
+}
